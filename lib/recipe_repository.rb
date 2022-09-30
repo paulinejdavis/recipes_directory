12518cbae63dc8c2d2e1edaa523ac41ed409be7b
Pauline Davis <pauljdavi.t21@btinternet.com>
2022-09-30 22:00:33 +0100

    🌶️ rspec fixes

--- lib/recipe_repository.rb
+++ lib/recipe_repository.rb
@@ -2,38 +2,36 @@ require_relative '../lib/recipe'
 
 class RecipeRepository
     def all
-      sql = 'SELECT id, name, cooking_time, rating FROM recipes;'
-      result_set = DatabaseConnection.exec_params(sql, [])
-  
-      recipes = []
-   
-  
-    result_set. each |record|
-    recipe = Recipe.new
-    recipe.id = record['id']
-    recipe.name = record['name']
-    recipe.cooking_time = record['cooking_time']
-    recipe.rating = record['rating']
+        recipes = []
 
-    recipes << recipe
-    
+        sql = 'SELECT id, name, cooking_time, rating FROM recipes;'
+        result_set = DatabaseConnection.exec_params(sql, [])
+        result_set.each do |record|
+            recipe = Recipe.new
+            recipe.id = record['id']
+            recipe.name = record['name']
+            recipe.cooking_time = record['cooking_time']
+            recipe.rating = record['rating']
+
+            recipes << recipe
+        end
+        return recipes
     end
-    return recipes
-end
+
 
     def find(id)
         sql = 'SELECT id, name, cooking_time, rating FROM recipes WHERE id = $1;'
-        sql_ref = [id]
-
-        result_set = DatabaseConnection.exec_params(sql, sql_ref)
-
+        sql_params = [id]
+        
+        result_set = DatabaseConnection.exec_params(sql, sql_params)
         record = result_set[0]
+
         recipe = Recipe.new
         recipe.id = record['id']
         recipe.name = record['name']
         recipe.cooking_time = record['cooking_time']
         recipe.rating = record['rating']
-
+        
         return recipe
     end
 end
--- spec/recipe_repository_spec.rb
+++ spec/recipe_repository_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../lib/recipe'
 require_relative '../lib/recipe_repository'
 
-RSpec.describe RecipesRepository do
+RSpec.describe RecipeRepository do
     def reset_recipes_table
         seed_sql = File.read('spec/seeds_recipes.sql')
         connection = PG.connect({ host: '127.0.0.1', dbname: 'recipes_directory_test' })
@@ -17,48 +17,48 @@ RSpec.describe RecipesRepository do
       repo = RecipeRepository.new
       recipes = repo.all
 
-      expect(recipes.length).to eq(4)
-      expect(recipes[0].id).to eq(1) 
-      expect(recipes[0].name) .to eq ("Tofu Surprise")
-      expect(recipes[0].cooking_tim) .to eq (5)
-      expect(recipes[0].rating) .to eq (4)
+      expect(recipes.length).to eq 4
+      expect(recipes[0].id).to eq "1"
+      expect(recipes[0].name) .to eq "Tofu Surprise"
+      expect(recipes[0].cooking_time) .to eq "5"
+      expect(recipes[0].rating) .to eq "4"
 
-      expect(recipes[1].id).to eq(2) # =>  2
-      expect(recipes[1].name).to eq("Ratatouille") # =>  "Ratatouille"
-      expect(recipes[1].cooking_time).to eq(10) # =>  10
-      expect(recipes[1].rating).to eq(3) # => 3
+      expect(recipes[1].id).to eq "2" 
+      expect(recipes[1].name).to eq "Ratatouille"
+      expect(recipes[1].cooking_time).to eq "10" 
+      expect(recipes[1].rating).to eq "3" 
 
-      expect(recipes[2].id).to eq(3)# =>  3
-      expect(recipes[2].name).to eq("Turkey Sandwich") # =>  "Turkey Sandwich"
-      expect(recipes[2].cooking_time).to eq (6)# =>  6
-      expect(recipes[2].rating).to eq(2) # => 2
+      expect(recipes[2].id).to eq "3"
+      expect(recipes[2].name).to eq "Turkey Sandwich" 
+      expect(recipes[2].cooking_time).to eq "6"
+      expect(recipes[2].rating).to eq "2" 
 
-      expect(recipes[3].id).to eq(4)# =>  4
-      expect(recipes[3].name).to eq("Waffles") # =>  "Waffles"
-      expect(recipes[3].cooking_time).to eq(5) # =>  5
-      expect(recipes[3].rating).to eq(5) # => 5
+      expect(recipes[3].id).to eq "4"
+      expect(recipes[3].name).to eq "Waffles" 
+      expect(recipes[3].cooking_time).to eq "5" 
+      expect(recipes[3].rating).to eq "5"
     end
-    # 2
-    it 'return a single recipe' do
-
+    
+    it 'return a single recipe tofu surprise' do
       repo = RecipeRepository.new
+      
       recipe = repo.find(1)
 
-      expect(recipe.id).to eq(1) # =>  1
-      expect(recipe.name).to eq("Tofu Surprise")# =>  'Tofu Surprise'
-      expect(recipe.cooking_time).to eq(5) # =>  5
-      expect(recipe.rating).to eq(4) # =>  4
+      expect(recipe.id).to eq "1" 
+      expect(recipe.name).to eq "Tofu Surprise"
+      expect(recipe.cooking_time).to eq "5" 
+      expect(recipe.rating).to eq "4"
     end
 
-    # 4
+    
     it 'returns the second recipe - Ratatouille' do
 
       repo = RecipeRepository.new
       recipe = repo.find(2)
 
-      expect(recipe.id).to eq(2) # =>  2
-      expect(recipe.name.to eq("Ratatouille") # =>  'Ratatouille'
-      expect(recipe.cooking_time).to eq(10) # =>  10
-      expect(recipe.rating).to eq(4) # =>  4
+      expect(recipe.id).to eq "2" 
+      expect(recipe.name).to eq "Ratatouille" 
+      expect(recipe.cooking_time).to eq "10" 
+      expect(recipe.rating).to eq "3" 
     end
 end
